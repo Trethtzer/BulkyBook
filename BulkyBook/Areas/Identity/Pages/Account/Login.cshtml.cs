ea15906a9b586868461aa29a6e0dfad90bbfd64f
Trethtzer <gsilesbonilla@gmail.com>
2020-11-16 13:16:32 +0100

    Failed login bug solved

--- BulkyBook/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ BulkyBook/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -80,6 +80,7 @@ namespace BulkyBook.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl = returnUrl ?? Url.Content("~/");
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             if (ModelState.IsValid)
             {
